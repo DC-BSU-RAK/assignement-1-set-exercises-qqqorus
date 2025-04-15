b2258cd1a3685ac74bbbcf794e6f956b3adb1314
Quishia Desiree Famador <httpsqorus@gmail.com>
2025-04-15 21:11:42 +0400

    ch6 update 5
    started with the javascript, created constant variables for the ids that were sourced from the html file

---
+++ Chapter-6-Javascript/Exercises/Petrol Calculator/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the references to the DOM elements which will be used here
+    const costPerLiterInput = document.getElementById('cost-per-liter');
+    const litersInput = document.getElementById('liters');
+    const calculateBtn = document.getElementById('calculate-btn');
+    const resultDisplay = document.getElementById('result');
+});
